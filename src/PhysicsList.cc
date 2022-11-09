7abf95f9b49dfba317e726a3c83d9163c436b483
LB <lg9783@bristol.ac.uk>
2022-11-09 22:11:13 +0000

    make to EmStandard physics

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -31,30 +31,38 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #include "PhysicsList.hh"
-#include "G4EmDNAPhysics_option2.hh"
-#include "G4EmDNAPhysicsActivator.hh"
+// #include "G4EmDNAPhysics_option2.hh"
+// #include "G4EmDNAPhysicsActivator.hh"
 
 #include "G4SystemOfUnits.hh"
 #include "G4UnitsTable.hh"
 
 #include "G4EmStandardPhysics.hh"
-#include "G4EmExtraPhysics.hh"
+// #include "G4EmExtraPhysics.hh"
 #include "G4EmParameters.hh"
 #include "G4DecayPhysics.hh"
 #include "G4RadioactiveDecayPhysics.hh"
 
-#include "G4HadronElasticPhysicsHP.hh"
-#include "G4HadronPhysicsFTFP_BERT_HP.hh"
-#include "G4HadronPhysicsQGSP_BIC_HP.hh"
-#include "G4HadronInelasticQBBC.hh"
-#include "G4HadronPhysicsINCLXX.hh"
-#include "G4IonElasticPhysics.hh"
-#include "G4IonPhysics.hh"
-#include "G4IonINCLXXPhysics.hh"
+// #include "G4HadronElasticPhysicsHP.hh"
+// #include "G4HadronPhysicsFTFP_BERT_HP.hh"
+// #include "G4HadronPhysicsQGSP_BIC_HP.hh"
+// #include "G4HadronInelasticQBBC.hh"
+// #include "G4HadronPhysicsINCLXX.hh"
+// #include "G4IonElasticPhysics.hh"
+// #include "G4IonPhysics.hh"
+// #include "G4IonINCLXXPhysics.hh"
 
 // particles
 
+// #include "G4BosonConstructor.hh"
+// #include "G4LeptonConstructor.hh"
+// #include "G4MesonConstructor.hh"
+// #include "G4BosonConstructor.hh"
+// #include "G4BaryonConstructor.hh"
+// #include "G4IonConstructor.hh"
+// #include "G4ShortLivedConstructor.hh"
 
+// #include "G4StepLimiterPhysics.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -64,20 +72,42 @@ PhysicsList::PhysicsList()
   G4int verb = 1;
   SetVerboseLevel(verb);
 
-    SetDefaultCutValue(1.0 * micrometer);
-  UseCoupledTransportation();
-  
   //add new units for radioActive decays
   //
 //   new G4UnitDefinition( "millielectronVolt", "meV", "Energy", 1.e-3*eV);   
   // EM physics
-  RegisterPhysics(new G4EmDNAPhysics_option2()); 
+  RegisterPhysics(new G4EmStandardPhysics());
+
+  G4EmParameters* param = G4EmParameters::Instance();
+  param->SetAugerCascade(true);
+  param->SetStepFunction(1., 1*CLHEP::mm);
+  param->SetStepFunctionMuHad(1., 1*CLHEP::mm);
+ 
+  // Decay
   RegisterPhysics(new G4DecayPhysics());
+
+  // Radioactive decay
   RegisterPhysics(new G4RadioactiveDecayPhysics());
 
-    G4ProductionCutsTable::GetProductionCutsTable()->SetEnergyRange(100 * eV,
-                                                                  1 * GeV);
-
+  // RegisterPhysics(new G4StepLimiterPhysics());
+            
+  // Hadron Elastic scattering
+  // RegisterPhysics( new G4HadronElasticPhysicsHP(verb) );
+  
+  // // Hadron Inelastic physics
+  // RegisterPhysics( new G4HadronPhysicsFTFP_BERT_HP(verb));
+  
+  // // Ion Elastic scattering
+  // RegisterPhysics( new G4IonElasticPhysics(verb));
+      
+  // // Ion Inelastic physics
+  // RegisterPhysics( new G4IonPhysics(verb));
+    
+  // // Gamma-Nuclear Physics
+  // G4EmExtraPhysics* gnuc = new G4EmExtraPhysics(verb);
+  // gnuc->ElectroNuclear(false);
+  // gnuc->MuonNuclear(false);
+  // RegisterPhysics(gnuc);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -87,6 +117,26 @@ PhysicsList::~PhysicsList()
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
+// void PhysicsList::ConstructParticle()
+// {
+//   G4BosonConstructor  pBosonConstructor;
+//   pBosonConstructor.ConstructParticle();
+
+//   G4LeptonConstructor pLeptonConstructor;
+//   pLeptonConstructor.ConstructParticle();
+
+//   G4MesonConstructor pMesonConstructor;
+//   pMesonConstructor.ConstructParticle();
+
+//   G4BaryonConstructor pBaryonConstructor;
+//   pBaryonConstructor.ConstructParticle();
+
+//   G4IonConstructor pIonConstructor;
+//   pIonConstructor.ConstructParticle();
+
+//   G4ShortLivedConstructor pShortLivedConstructor;
+//   pShortLivedConstructor.ConstructParticle();  
+// }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
