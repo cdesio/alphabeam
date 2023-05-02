41f9846e9e378ea95e503f329f8c8c9b63044112
LB <lg9783@bristol.ac.uk>
2023-05-02 11:07:37 +0100

    correct margin, increase world

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -103,7 +103,7 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
   G4Box *solidWorld = new G4Box("world", 100 * mm, 100 * mm, 100 * mm);
 
-  G4Box *solidWater = new G4Box("water", Rmax + 1 * mm, Rmax + 1 * mm, 10 * mm); // reduce x/y size of volume where particles are tracked
+  G4Box *solidWater = new G4Box("water", 10 * mm, 10 * mm, 10 * mm); // reduce x/y size of volume where particles are tracked
 
   G4Tubs *solidSeed = new G4Tubs("seed", 0., 0.15 * mm, 3 * mm, 0, 360 * degree);
   // G4Box *solidCell = new G4Box("cell", nucleusSize/2+ margin, nucleusSize/2 + margin, nucleusSize/2+ margin);
@@ -151,7 +151,7 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
   for (G4int r = 0; r < R.size(); ++r)
   {
-        G4Tubs *solidCell = new G4Tubs("seed", R[r]-150*nm, R[r]+150*nm, 3 * mm, 0, 360 * degree);
+        G4Tubs *solidCell = new G4Tubs("seed", R[r]-(nucleusSize/2+ margin), R[r]+(nucleusSize/2+ margin), 3 * mm, 0, 360 * degree);
 
         G4LogicalVolume *logicCell = new G4LogicalVolume(solidCell,
                                                          waterMaterial,
